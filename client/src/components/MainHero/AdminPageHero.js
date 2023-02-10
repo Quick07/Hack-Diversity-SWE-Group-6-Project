a9f2854ef36bfff05e12188002dfe3dfa405d89e
titamah <titamah@icloud.com>
2023-02-10 09:18:49 -0500

    updating admin and index page heros

--- client/src/components/MainHero/AdminPageHero.js
+++ client/src/components/MainHero/AdminPageHero.js
@@ -3,8 +3,7 @@ import './MainHero.css';
 
 export default function AdminPageHero() {
   return (
-    <div className='MainHeroContainer'>
-      <h1 className='header'>Welcome to the Admin Page</h1>
+    <div className='AdminHeroContainer'>
     </div>
   );
 }
--- client/src/components/MainHero/MainHero.js
+++ client/src/components/MainHero/MainHero.js
@@ -4,7 +4,7 @@ import './MainHero.css';
 export default function MainHero() {
   return (
     <div className='MainHeroContainer'>
-      <h1 className='header'>Welcome to Advance X-Ray</h1>
+      Welcome to Advance X-Ray
     </div>
   );
 }
