5ea8437920fb09d8419d0a3316cde5367889b910
johnfleurimond <johncfleurimond@gmail.com>
2023-02-23 13:34:34 -0500

    changes john made

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,14 +1,9 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-<<<<<<< HEAD
-router.get("/", function (req, res, next) {
-  res.send("API is working properly!");
-=======
 router.get('/', function (req, res, next) {
   res.send('API is working properly!');
->>>>>>> main
 });
 
 module.exports = router;
