677e1b6a2b00a97de805dc0d01e01af51f265a77
LosmanDev <losman78@gmail.com>
2023-03-06 14:06:27 -0500

    edit for mobile repsonsive

--- client/src/components/AboutPage/AboutPage.js
+++ client/src/components/AboutPage/AboutPage.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import './AboutPage.css';
-import Button from '../Buttons/Button';
 export default function AboutPage() {
   return (
-    <div className='aboutcontentcontainer'>
-      <h1 className='aboutheader'>Contributors</h1>
-      <div>
-        <h3 className='aboutheader2'>John Fleurimond</h3>
-        <h3 className='aboutheader2'>Luz Agugelo</h3>
-        <h3 className='aboutheader2'>Donovan Francis</h3>
-        <h3 className='aboutheader2'>Liban Osman</h3>
-        <h3 className='aboutheader2'>Nick Johnson</h3>
-        <h3 className='aboutheader2'>Titamah Simpson</h3>
+    <div className='aboutpage'>
+      <div className='aboutcontentcontainer'>
+        <h1 className='aboutheader'>Contributors</h1>
+        <div>
+          <h3 className='aboutheader2'>John Fleurimond</h3>
+          <h3 className='aboutheader2'>Luz Agudelo</h3>
+          <h3 className='aboutheader2'>Donovan Francis</h3>
+          <h3 className='aboutheader2'>Liban Osman</h3>
+          <h3 className='aboutheader2'>Nick Johnson</h3>
+          <h3 className='aboutheader2'>Titamah Simpson</h3>
+        </div>
+        <a
+          target='_blank'
+          href='https://github.com/Quick07/Hack-Diversity-SWE-Group-6-Project'
+        >
+          <button className='Githubbtn'>Github</button>
+        </a>
       </div>
-      <form
-        action='https://github.com/Quick07/Hack-Diversity-SWE-Group-6-Project'
-        method='get'
-        target='_blank'
-      >
-        <Button button='Github' />
-      </form>
     </div>
   );
 }
