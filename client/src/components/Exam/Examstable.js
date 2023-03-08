aaf7710bdaf4aa7190e163716f21b77cf5bb6e21
tkmariana <57637306+tkmariana@users.noreply.github.com>
2023-03-08 01:54:06 -0500

    Update Examstable.js
    connected backend with exam table

--- client/src/components/Exam/Examstable.js
+++ client/src/components/Exam/Examstable.js
@@ -29,28 +29,18 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-function createData(
-  PatientID,
-  ExamID,
-  Image,
-  KeyFindings,
-  Age,
-  Sex,
-  BMI,
-  ZipCode
-) {
-  return { PatientID, ExamID, Image, KeyFindings, Age, Sex, BMI, ZipCode };
-}
-
-const rows = [
-  createData(1, '000', 1, 6.0, 24, 'F', 200, 1),
-  createData(2, '001', 2, 9.0, 37, 'M', 1, 2),
-  createData(3, '002', 3, 16.0, 24, 'F', 2, 3),
-  createData(4, '003', 4, 3.7, 67, 'F', 3, 4),
-  createData(5, '004', 5, 16.0, 49, 'M', 4, 5),
-];
 
 export default function Examstable() {
+  const [rows, setRows] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:9000/exams')
+      .then(res => res.json())
+      .then(data => setRows(data))
+      .catch(err => console.error(err));
+     
+  }, []);
+
   return (
       <TableContainer component={Paper}>
         <Table
@@ -70,28 +60,25 @@ export default function Examstable() {
               <StyledTableCell align='left'>Exam ID</StyledTableCell>
               <StyledTableCell align='left'>Image</StyledTableCell>
               <StyledTableCell align='left'>Key Findings</StyledTableCell>
-              <StyledTableCell align='left'>Age</StyledTableCell>
-              <StyledTableCell align='left'>Sex</StyledTableCell>
-              <StyledTableCell align='left'>BMI</StyledTableCell>
-              <StyledTableCell align='left'>Zip Code</StyledTableCell>
+              <StyledTableCell align='left'>brixia_scores</StyledTableCell>
+             
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map(row => (
-              <StyledTableRow key={row.PatientID}>
+              <StyledTableRow key={row.PATIENT_ID}>
                 <StyledTableCell component='th' scope='row'>
-                  {row.PatientID}
+                  {row.PATIENT_ID}
                 </StyledTableCell>
 
-                <StyledTableCell align='left'>{row.ExamID}</StyledTableCell>
-                <StyledTableCell align='left'>{row.image}</StyledTableCell>
+                <StyledTableCell align='left'>{row.exam_Id}</StyledTableCell>
                 <StyledTableCell align='left'>
-                  {row.KeyFindings}
+                 <img src={row.xray_url} alt='xray' style={{ maxWidth: '70px', }} />
                 </StyledTableCell>
-                <StyledTableCell align='left'>{row.Age}</StyledTableCell>
-                <StyledTableCell align='left'>{row.Sex}</StyledTableCell>
-                <StyledTableCell align='left'>{row.BMI}</StyledTableCell>
-                <StyledTableCell align='left'>{row.ZipCode}</StyledTableCell>
+                <StyledTableCell align='left'>
+                  {row.key_findings}
+                </StyledTableCell>
+                <StyledTableCell align='left'>{row.brixia_scores}</StyledTableCell>
               </StyledTableRow>
             ))}
           </TableBody>
